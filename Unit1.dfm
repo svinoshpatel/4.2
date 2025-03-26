70303bb4c746ae10825fb546b3532636861df999
svinoshpatel <132487936+svinoshpatel@users.noreply.github.com>
2025-03-26 07:05:05 -0700

    Add files via upload

---
+++ Unit1.dfm
@@ -0,0 +1,86 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 323
+  ClientWidth = 504
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  TextHeight = 15
+  object Label1: TLabel
+    Left = 32
+    Top = 16
+    Width = 153
+    Height = 15
+    Caption = #1042#1074#1077#1076#1110#1090#1100' '#1055#1088#1110#1079#1074#1080#1097#1077' '#1090#1072' '#1030#1084#39#1103
+  end
+  object Label2: TLabel
+    Left = 32
+    Top = 80
+    Width = 197
+    Height = 15
+    Caption = #1042#1074#1077#1076#1110#1090#1100' '#1053#1072#1079#1074#1091' '#1082#1072#1092#1077#1076#1088#1080' '#1072#1073#1086' N '#1075#1088#1091#1087#1080' '
+  end
+  object Edit1: TEdit
+    Left = 32
+    Top = 37
+    Width = 433
+    Height = 23
+    TabOrder = 0
+  end
+  object Edit2: TEdit
+    Left = 32
+    Top = 101
+    Width = 433
+    Height = 23
+    TabOrder = 1
+  end
+  object Button1: TButton
+    Left = 344
+    Top = 176
+    Width = 75
+    Height = 25
+    Caption = #1044#1086#1076#1072#1090#1080
+    TabOrder = 2
+    OnClick = Button1Click
+  end
+  object Button2: TButton
+    Left = 336
+    Top = 240
+    Width = 97
+    Height = 25
+    Caption = #1044#1088#1091#1082' '#1089#1087#1080#1089#1082#1091
+    TabOrder = 3
+    OnClick = Button2Click
+  end
+  object GroupBox1: TGroupBox
+    Left = 24
+    Top = 192
+    Width = 185
+    Height = 105
+    Caption = #1054#1073#1077#1088#1110#1090#1100
+    TabOrder = 4
+    object RadioButton1: TRadioButton
+      Left = 16
+      Top = 32
+      Width = 113
+      Height = 17
+      Caption = #1057#1090#1091#1076#1077#1085#1090
+      Checked = True
+      TabOrder = 0
+      TabStop = True
+    end
+    object RadioButton2: TRadioButton
+      Left = 16
+      Top = 64
+      Width = 113
+      Height = 17
+      Caption = #1042#1080#1082#1083#1072#1076#1072#1095
+      TabOrder = 1
+    end
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,114 @@
+﻿unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Label1: TLabel;
+    Edit1: TEdit;
+    Label2: TLabel;
+    Edit2: TEdit;
+    Button1: TButton;
+    Button2: TButton;
+    GroupBox1: TGroupBox;
+    RadioButton1: TRadioButton;
+    RadioButton2: TRadioButton;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+  TPerson = class // базовий клас-предок
+ fName: String; // ім'я особи
+ Constructor Create (name: String);
+Function Info: String; Virtual;
+end;
+TStud = class (TPerson) // клас-нащадок Студент
+ fGr: Integer; // номер групи
+ Constructor Create (name: String; gr: Integer);
+ Function Info: String; Override;
+end;
+TProf = class (TPerson) // клас-нащадок Викладач
+ fdep: String; // назва кафедри
+ Constructor Create (name: String; dep: String);
+ Function Info: String; Override;
+end;
+
+Const SZL = 10;
+
+
+var
+  Form1: TForm1;
+  List: Array[1..SZL] of TPerson; // список осіб
+ n: Integer = 0; // початкова кількість осіб у списку
+
+implementation
+
+{$R *.dfm}
+
+Constructor TPerson.Create (name: String);
+Begin
+fName := name;
+end;
+Constructor TStud.Create(name: String; gr: Integer);
+Begin
+Inherited Create(name); // виклик конструктора класу-предка
+fGr := gr;
+end;
+Constructor TProf.Create(name: String; dep: String);
+Begin
+Inherited Create(name); // виклик конструктора класу-предка
+fDep := dep;
+end;
+Function TPerson.Info: String;
+Begin
+ Result := fName;
+end;
+Function TStud.Info: String;
+Begin
+ Result := fName + ' ФЕІ - ' + IntToStr(fGr);
+end;
+Function TProf.Info:string;
+Begin
+ Result := fName + ' Викладач кафедри ' + fDep;
+end;
+
+
+Procedure TForm1.Button1Click (Sender: TObject);
+Begin
+If n < SZL then
+Begin // додаємо особу до списку
+ n := n + 1;
+ If RadioButton1.Checked then
+// Створення об'єкту TStud
+ List[n] := TStud.Create(Edit1.Text, StrToInt(Edit2.Text))
+ else
+// Створення об'єкту TProf
+List[n] := TProf.Create(Edit1.Text, Edit2.Text);
+// Очищення поля вводу
+ Edit1.Text := '';
+ Edit2.Text := '';
+ Edit1.SetFocus; // курсор ставимо у поле Прізвище
+end
+else ShowMessage('Список заповнено!');
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+Var i: Integer; // індекс
+ st: String; // список
+Begin
+  For i:=1 to SZL do
+ If list[i] <> NIL then
+ st := st + list[i].Info + #13;
+ ShowMessage(' Список студентів та викладачів' + #13 + st);
+end;
+end.
+End;
+
+end.
